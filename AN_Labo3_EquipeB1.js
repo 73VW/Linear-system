88e89fbafa960b06e982eb1c51ce2c6b89c7f43b
axel.rieben <axel.rieben@he-arc.ch>
2017-04-16 17:55:25 +0200

    Show the matrix, resolved bug, check if the system has no solution

--- AN_Labo3_EquipeB1.js
+++ AN_Labo3_EquipeB1.js
@@ -8,103 +8,136 @@
 /*******************************************************/
 
 function $(id) {
-  return document.getElementById(id);
+    return document.getElementById(id);
 }
 
 function $name(name) {
-  return document.getElementsByName(name);
+    return document.getElementsByName(name);
 }
 
-
-
+//Thread
 var w;
 var w2;
 
 function runThread() {
-  if (typeof(Worker) !== "undefined") {
-    console.log("Browser supported");
-    if (typeof(w) == "undefined") {
-      w = new Worker("worker.js");
-      w.postMessage("Bonjour");
+    if (typeof(Worker) !== "undefined") {
+        console.log("Browser supported");
+        if (typeof(w) == "undefined") {
+            w = new Worker("worker.js");
+            w.postMessage("Bonjour");
+        }
+        w.onmessage = function(event) {
+            $("result").innerHTML += "<br/>" + event.data;
+        };
+        w.onerror = function(event) {
+            $("result").innerHTML += "<br/>" + event.message;
+        };
+    } else {
+        $("result").innerHTML = "Sorry, your browser does not support Web Workers...";
     }
-    w.onmessage = function(event) {
-      $("result").innerHTML += "<br/>" + event.data;
-    };
-    w.onerror = function(event) {
-      $("result").innerHTML += "<br/>" + event.message;
-    };
-  } else {
-    $("result").innerHTML = "Sorry, your browser does not support Web Workers...";
-  }
 }
 
 function sendMessage() {
-  if (typeof(w) != "undefined")
-  w.postMessage($('text').value);
+    if (typeof(w) != "undefined")
+        w.postMessage($('text').value);
 }
 
 function stopThread() {
-  if (typeof(w) != "undefined") {
-    w.terminate();
-    w = undefined;
-  }
+    if (typeof(w) != "undefined") {
+        w.terminate();
+        w = undefined;
+    }
 }
 
 //---------------------------------------------------------------------------------------------------------
 
 // Source: https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
 function loadJSON(callback) {
-  var xobj = new XMLHttpRequest();
-  xobj.overrideMimeType("application/json");
-  xobj.open('GET', 'matrix/matrice_300x300.json', true);
-  xobj.onreadystatechange = function () {
-    if (xobj.readyState == 4 && xobj.status == "200") {
-      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-      callback(xobj.responseText);
-    }
-  };
-  xobj.send(null);
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'matrix/matrice_3x3.json', true);
+    xobj.onreadystatechange = function() {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
 }
 
 function loadMatrixFromJSON() {
-  loadJSON(function(response) {
-    // Parse JSON string into object
-    var json = JSON.parse(response);
-    buildMatrixFromJSON(json);
-  });
+    loadJSON(function(response) {
+        // Parse JSON string into object
+        var json = JSON.parse(response);
+        buildMatrixFromJSON(json);
+    });
 }
 
-/* build a matrix from the JSON file, the resulting matrix is of the following form: (x1, y1, z1, constant1)
-(x2, y2, z2, constant2)*/
+/* build a matrix from the JSON file, the resulting matrix is of the following form:
+(x1, y1, z1, constant1)
+(x2, y2, z2, constant2)
+*/
 function buildMatrixFromJSON(json) {
-  var matrix = new Array();
-  var aIndex = 0;
-  var bIndex = 0;
-  for (var i = 0; i < json.n; i++) {
-    matrix[i] = new Array();
-    for (var j = 0; j < json.n; j++) {
-      matrix[i].push(parseInt(json.A[aIndex]));
-      aIndex++;
+    let n = json.n;
+    var matrix = new Array();
+    var aIndex = 0;
+    var bIndex = 0;
+    for (var i = 0; i < n; i++) {
+        matrix[i] = new Array();
+        for (var j = 0; j < n; j++) {
+            matrix[i].push(parseFloat(json.A[aIndex]));
+            aIndex++;
+        }
+        matrix[i].push(parseFloat(json.B[bIndex]));
+        bIndex++;
     }
-    matrix[i].push(parseInt(json.B[bIndex]));
-    bIndex++;
-  }
 
-  solve(matrix);
+    showMatrix(matrix);
+    solve(matrix);
+}
+
+//Show the matrix in the HTML page
+function showMatrix(matrix) {
+    let n = matrix.length;
+    $('matrix').innerHTML = "";
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            $('matrix').innerHTML += matrix[i][j] + " ";
+        }
+        $('matrix').innerHTML += " = " + matrix[i][n] + " ";
+        $('matrix').innerHTML += "</br>";
+    }
+}
+
+//Show the solutions in the HTML page
+function showResult(solutions) {
+    //Check if the solution has been found
+    let noSolution = false;
+    for (var i = 0; i < solutions.length; i++) {
+        if (!solutions[i]) {
+            noSolution = true;
+        }
+    }
+
+    if (!noSolution) {
+        $('result').innerHTML = "";
+        $('result').innerHTML += "</br>"
+        for (var i = 0; i < solutions.length; i++) {
+            $('result').innerHTML += "x<sub>" + i + "</sub> = " + solutions[i] + "</br>";
+        }
+    } else {
+        $('result').innerHTML = "The aforementioned equation system can not be solved"
+    }
 }
 
 // Start the timer, solve the linear system and then stop the timer. Finally display the results on the screen.
 function solve(matrix) {
-  var start = Date.now();
-  var solutions = gauss(matrix);
-  var time = Date.now() - start;
+    let start = Date.now();
+    let solutions = gauss(matrix);
+    let time = Date.now() - start;
 
-  $('result').innerHTML = "";
-  for (var i = 0; i < solutions.length; i++) {
-    $('result').innerHTML += solutions[i] + ", ";
-  }
-
-  $('time').innerHTML = time + "ms";
+    showResult(solutions);
+    $('time').innerHTML = time + "ms";
 }
 
 /* Source: https://martin-thoma.com/solving-linear-equations-with-gaussian-elimination/#javascript-code
@@ -113,46 +146,47 @@ The algorithm comes from the above source, but has been slightly modidfied.
 Solve a linear system of equations given by a n*n matrix
 with a result vector n*1. */
 function gauss(matrix) {
-  var n = matrix.length;
-  for (var currentPos = 0; currentPos < n; currentPos++) {
-    // Search for maximum in this column
-    var maxRowValue = Math.abs(matrix[currentPos][currentPos]);
-    var maxRowIndex = currentPos;
-    for(var currentRow = currentPos+1; currentRow < n; currentRow++) {
-      if (Math.abs(matrix[currentRow][currentPos]) > maxRowValue) {
-        maxRowValue = Math.abs(matrix[currentRow][currentPos]);
-        maxRowIndex = currentRow;
-      }
-    }
+    let n = matrix.length;
 
-    // Swap maximum row with current row (column by column)
-    for (var currentColumn = currentPos; currentColumn < n+1; currentColumn++) {
-      var tmp = matrix[maxRowIndex][currentColumn];
-      matrix[maxRowIndex][currentColumn] = matrix[currentPos][currentColumn];
-      matrix[currentPos][currentColumn] = tmp;
-    }
-
-    // Make all rows below this one 0 in current column
-    for (currentRow = currentPos+1; currentRow < n; currentRow++) {
-      var c = -matrix[currentRow][currentPos]/matrix[currentPos][currentPos];
-      for(var currentColumn = currentPos; currentColumn < n+1; currentColumn++) {
-        if (currentPos == currentColumn) {
-          matrix[currentRow][currentColumn] = 0;
-        } else {
-          matrix[currentRow][currentColumn] += c * matrix[currentPos][currentColumn];
+    for (let currentPos = 0; currentPos < n; currentPos++) {
+        // Search for maximum in this column
+        let maxRowValue = Math.abs(matrix[currentPos][currentPos]);
+        let maxRowIndex = currentPos;
+        for (let currentRow = currentPos + 1; currentRow < n; currentRow++) {
+            if (Math.abs(matrix[currentRow][currentPos]) > maxRowValue) {
+                maxRowValue = Math.abs(matrix[currentRow][currentPos]);
+                maxRowIndex = currentRow;
+            }
         }
-      }
-    }
-  }
 
-  // Solve equation Ax=b for an upper triangular matrix A (substitution)
-  var solutions = new Array(n);
-  for (var currentPos = n-1; currentPos > -1; currentPos--) {
-    solutions[currentPos] = matrix[currentPos][n]/matrix[currentPos][currentPos];
-    for (var currentRow = currentPos-1; currentRow > -1; currentRow--) {
-      matrix[currentRow][n] -= matrix[currentRow][currentPos] * solutions[currentPos];
-    }
-  }
+        // Swap maximum row with current row (column by column)
+        for (let currentColumn = currentPos; currentColumn < n + 1; currentColumn++) {
+            let tmp = matrix[maxRowIndex][currentColumn];
+            matrix[maxRowIndex][currentColumn] = matrix[currentPos][currentColumn];
+            matrix[currentPos][currentColumn] = tmp;
+        }
 
-  return solutions;
+        // Make all rows below this one 0 in current column
+        for (currentRow = currentPos + 1; currentRow < n; currentRow++) {
+            let c = -matrix[currentRow][currentPos] / matrix[currentPos][currentPos];
+            for (let currentColumn = currentPos; currentColumn < n + 1; currentColumn++) {
+                if (currentPos == currentColumn) {
+                    matrix[currentRow][currentColumn] = 0;
+                } else {
+                    matrix[currentRow][currentColumn] += c * matrix[currentPos][currentColumn];
+                }
+            }
+        }
+    }
+
+    // Solve equation Ax=b for an upper triangular matrix A (substitution)
+    let solutions = new Array(n);
+    for (let currentPos = n - 1; currentPos > -1; currentPos--) {
+        solutions[currentPos] = matrix[currentPos][n] / matrix[currentPos][currentPos];
+        for (let currentRow = currentPos - 1; currentRow > -1; currentRow--) {
+            matrix[currentRow][n] -= matrix[currentRow][currentPos] * solutions[currentPos];
+        }
+    }
+
+    return solutions;
 }
